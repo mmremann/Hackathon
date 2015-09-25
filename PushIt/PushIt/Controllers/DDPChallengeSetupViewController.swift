9a1af4ddcd2cce5bafd4927beb03f1116c30ef31
Josh Mann <josmann@deloitte.com>
2015-09-25 15:39:25 -0600

    getting smokey

---
+++ PushIt/PushIt/Controllers/DDPChallengeSetupViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DDPChallengeSetupViewController.swift
+//  PushIt
+//
+//  Created by Mann, Josh (US - Denver) on 9/25/15.
+//  Copyright © 2015 Mann, Josh (US - Denver). All rights reserved.
+//
+
+import UIKit
+
+class DDPChallengeSetupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pushItRealGoodButtonPressed(sender: AnyObject) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
